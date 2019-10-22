fa66755eef0553807771a5cd6d9b603a571473b6
Kuledude69 <56176588+Kuledude69@users.noreply.github.com>
2019-10-22 15:05:31 -0700

    10/22/19
    wakanaiyo

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -19,6 +19,7 @@ public class StudentRecordTester
         students[0] = new StudentRecord(a);
         students[1] = new StudentRecord(b);
         students[2] = new StudentRecord(c);
+        
     }
     
 }
